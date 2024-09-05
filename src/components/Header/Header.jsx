79ccc265bc7a3e8336720a5643c1b818afa89a35
FarhanBeacon <farhanahmedbeacon@gmail.com>
2024-09-06 00:22:19 +0600

    second commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl bg-orange-500 p-2'>Knowledge Cafe</h1>            
+        </div>
+    );
+};
+
+export default Header;
